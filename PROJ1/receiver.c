31f4c2ccf1e78e0c6ceaac8ed618d509cba80e5a
David Reis <david.alex.reis.97@gmail.com>
2017-10-09 21:49:32 +0100

    Added basic sender and receiver, able to support further development

--- PROJ1/receiver.c
+++ PROJ1/receiver.c
@@ -11,72 +11,28 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
-
 const char FLAG = 0x7E;
 
-const char RECEIVER_ADRESS = 0x01;
-const char SENDER_ADRESS = 0x03;
+const char SENDER_ADDRESS = 0x03;
+const char RECEIVER_ADDRESS = 0x01;
 
-const char CONTROL_SET = 0b00000011;
-const char CONTROL_DISC = 0b00001011;
-const char CONTROL_UA = 0b0000111;
-const char CONTROL_RR = 0b000101;
-const char CONTROL_REJ = 0b00001;
+const char CTRL_SET = 0b00000011;
+const char CTRL_DISC = 0b00001011;
+const char CTRL_UA = 0b00000111;
+const char CTRL_RR = 0b00000101;
+const char CTRL_REJ = 0b0000001;
 
-int process_frame( unsigned char* buf, int n){
+const int BUFFER_SIZE = 255;
 
-	if (buf[2] == CONTROL_SET){
-		buf[2] = CONTROL_UA;
-		printf("CHANGED \n ");
-		}
-
-			
-}
-
-int frame_reader(int fd, unsigned char* buf, int maxlen){
-
-	int n=0;
-	int ch;
-	
-	while(1){
-		if(ch=read(fd,buf+1,1))<=0)
-			return ch; // error;
-		if(n==0 && buf[n]!=FLAG)
-			continue;
-		if(n==1 && buf[n]=FLAG)
-			continue;
-		n++;
-		if(buf[n-1] != FLAG && n==maxlen){
-			n=0;
-			continue;
-		}
-		if(buf[n-1]== FLAG && n>2){
-		process_frame(buf,n);
-		return n;
-	}
-
-
-}
-
-
-void receiver(char* serialPort){
-
-int fd,c, res;
+void *run(void *(*f)(int, void *), void *args, char *port)
+{
+    int fd;
     struct termios oldtio,newtio;
-    char buf[255];
-
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
   
-    
-    fd = open(serialPort, O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(serialPort); exit(-1); }
+    fd = open(port, O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(port); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    if ( tcgetattr(fd,&oldtio) == -1) {
       perror("tcgetattr");
       exit(-1);
     }
@@ -86,20 +42,10 @@ int fd,c, res;
     newtio.c_iflag = IGNPAR;
     newtio.c_oflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-  */
-
-
+    newtio.c_cc[VTIME]    = 0;  
+    newtio.c_cc[VMIN]     = 1;
 
     tcflush(fd, TCIOFLUSH);
 
@@ -108,40 +54,46 @@ int fd,c, res;
       exit(-1);
     }
 
-    printf("New termios structure set\n");
-
-	
-    frame_reader(fd,buf,255);
+    void * retVal = f(fd,args);
     
-    
-
-
-
-  /* 
-    O ciclo WHILE deve ser alterado de modo a 
-    
-    respeitar o indicado no guião 
-  */
-
-
-
+    tcflush(fd,TCIOFLUSH);
     tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
-    return 0;
+    return retVal;
 }
 
- 
-int main(int argc, char** argv)
-{
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
+void * receiveAndProcess(int fd, void *args){
+    char *buffer = (char *) args;
+    int n = 0;
+    int ch;
+    while(1){
+        if((ch = read(fd,buffer+n,1)) <=0 ){
+            return ch; //Error case        
+        }else if (n==0 && buffer[0] != FLAG){
+            printf("No flag on initial byte.\n");
+            continue;
+        }else if (n==1 && buffer[1] == FLAG){
+            printf("Flag after first byte..\n");
+            continue;
+        }else if (buffer[n] != FLAG && n == BUFFER_SIZE - 1){ //Wraps around?...
+            printf("Exceeded buffer.\n");
+            n = 0;
+            continue;
+        }else if (buffer[n] == FLAG && n > 2){
+            processFrame(buffer,n+1);
+            return NULL;
+        }
+        n++;
+        printf("Finished loop peacefully.\n");
     }
-    receiver(argv[1]);
+}
 
-    
-    
+void processFrame(char *buffer, int n){
+    printf("Received a frame with %d bytes\n",n);
+}
+
+int main(){
+    char buffer[BUFFER_SIZE];
+    run(receiveAndProcess,buffer,"/dev/ttyS0");
 }
 
--- PROJ1/send.c
+++ PROJ1/send.c
@@ -1,3 +1,5 @@
+/*Non-Canonical Input Processing*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -5,6 +7,9 @@
 #include <stdio.h>
 
 #define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
 
 const char FLAG = 0x7E;
 
@@ -19,130 +24,61 @@ const char CTRL_REJ = 0b0000001;
 
 const int BUFFER_SIZE = 255;
 
-typedef enum status{STARTED, STOPPED, ENDED}STATUS;
+void *run(void *(*f)(int, void *), void *args, char *port)
+{
+    int fd;
+    struct termios oldtio,newtio;
+  
+    fd = open(port, O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(port); exit(-1); }
 
-unsigned int sendSET(char *port){
-	char buffer[BUFFER_SIZE];
-	bzero(buffer, 255);
-	buffer[0] = FLAG;
+    if ( tcgetattr(fd,&oldtio) == -1) {
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;  
+    newtio.c_cc[VMIN]     = 1;
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    void * retVal = f(fd,args);
+    
+    tcflush(fd,TCIOFLUSH);
+    tcsetattr(fd,TCSANOW,&oldtio);
+    close(fd);
+    return retVal;
+}
+
+void *createAndSend(int fd, void *args){
+    char buffer[BUFFER_SIZE];
+    bzero(buffer, 255);
+    
+    buffer[0] = FLAG;
 	buffer[1] = SENDER_ADDRESS;
 	buffer[2] = CTRL_SET;
 	buffer[3] = 0; //Block Check Character (BCC)
 	buffer[4] = FLAG;
-	buffer[5] = '\0';
-
-	int fd,c, res;
-	struct termios oldtio,newtio;
-	int i, sum = 0, speed = 0;
-
-	fd = open(port, O_RDWR | O_NOCTTY );
-	if (fd <0) {perror(port); exit(-1); }
-
-	if ( tcgetattr(fd,&oldtio) == -1) {
-		perror("tcgetattr");
-		exit(-1);
-	}
-
-	bzero(&newtio, sizeof(newtio));
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
-
-	newtio.c_lflag = 0;
-
-	newtio.c_cc[VTIME]    = 2;   
-	newtio.c_cc[VMIN]     = 10;
-
-	tcflush(fd, TCIOFLUSH);
-
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-		perror("tcsetattr");
-		exit(-1);
-	}
-
-	printf("New termios structure set\n");
-
-	res = write(fd,buffer,255);   
-	printf("%d bytes written\n", res);
-
-	if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-		perror("tcsetattr");
-		exit(-1);
-	}
-
-	close(fd);
-	return 0;
+    buffer[5] = '\0';
+    
+    int num = write(fd,buffer,5);
+    printf("Wrote %d bytes\n",num);
+    return NULL;
 }
 
-char *receive(char buffer[],char port[]){
-	int fd,c, res;
-	struct termios oldtio,newtio;
-
-	fd = open(port, O_RDWR | O_NOCTTY );
-	if (fd <0) {perror(port); exit(-1); }
-
-	if ( tcgetattr(fd,&oldtio) == -1) { 
-		perror("tcgetattr");
-		exit(-1);
-	}
-
-	bzero(&newtio, sizeof(newtio));
-	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
-
-	newtio.c_lflag = 0;
-
-	newtio.c_cc[VTIME]    = 0;    
-	newtio.c_cc[VMIN]     = 1;   
-
-	tcflush(fd, TCIOFLUSH);
-
-	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-		perror("tcsetattr");
-		exit(-1);
-	}
-
-	printf("New termios structure set\n");
-
-	int pos = 0;
-	STATUS st = STOPPED;
-
-	do {
-		char received;
-		int size = read(fd,&received,1);   
-
-		if(size == 1){
-			if(res == 0x7E){
-				if(st == STOPPED){
-					st = STARTED;
-				}else{
-					st = ENDED;
-				}
-			}
-			buffer[pos++] = received;
-		}
-	}while (st != ENDED);
-
-	buffer[pos++] = 0;
-
-	printf("0x%08x\n", buffer);
-
-	tcsetattr(fd,TCSANOW,&oldtio);
-	close(fd);
-	return buffer;
+int main(){
+    run(createAndSend,NULL,"/dev/ttyS0");
 }
 
-int main(int argc, char *argv[]){
-	if ( (argc < 2) || 
-		((strcmp("/dev/ttyS0", argv[1])!=0) && 
-			(strcmp("/dev/ttyS1", argv[1])!=0) )) {
-		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-	exit(1);
-	}
-
-	char buffer[BUFFER_SIZE];
-	receive(buffer, argv[1]);
-
-	return 0;
-}
